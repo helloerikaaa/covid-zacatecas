a828a23513fb12cd97e7dce2f762717bbabab454
Erika Sánchez Femat <erika.sanchez@bsc.es>
2020-08-08 20:25:15 -0500

    mend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,9 +4,16 @@
 
 export const environment = {
   production: false,
-  /*
-  * Add firebase keys for your proyect.
-  */
+  firebase: {
+    apiKey: "AIzaSyCA8bJHMekW8ix2CdmJan85mCdrMVDTNHA",
+    authDomain: "zac-covid.firebaseapp.com",
+    databaseURL: "https://zac-covid.firebaseio.com",
+    projectId: "zac-covid",
+    storageBucket: "zac-covid.appspot.com",
+    messagingSenderId: "311884672228",
+    appId: "1:311884672228:web:ff5c07d0759abcf20990f1",
+    measurementId: "G-525GLTK2NR"
+  },
 };
 
 /*
